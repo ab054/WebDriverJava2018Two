0895348f9dc36be8e75b0c05b8d392e9dab0c0df
ab054 <alfamiliya@gmail.com>
2018-08-27 22:08:47 -0700

    day2 progress

--- src/test/java/day2/Day2.java
+++ src/test/java/day2/Day2.java
@@ -1,10 +1,16 @@
 package day2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Day2 {
@@ -19,10 +25,24 @@ public class Day2 {
     }
 
     @Test
-    public void testCheckboxes() {
+    public void testGoogleSearch() {
         driver.get("http://google.com");
+        //wait(5);
 
+        By searchInput = By.cssSelector("#lst-ib");
+        By searchInputX = By.xpath("//*[@id=\"lst-ib\"]");
+
+        waitFor(searchInputX, 5);
+        driver.findElement(searchInputX).sendKeys("Portnov");
+        driver.findElement(searchInputX).submit();
         sleep(5);
+    }
+
+    @Test
+    public void testCheckboxes() {
+
+
+
     }
 
     @AfterTest
@@ -37,4 +57,14 @@ public class Day2 {
             e.printStackTrace();
         }
     }
+
+    private void wait(int seconds){
+        driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+    }
+
+    private void waitFor(By element, int seconds){
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        WebElement myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(element));
+    }
+
 }
