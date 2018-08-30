9c53e4e3082efd35e32226948e1083e9a10f4126
ab054 <alfamiliya@gmail.com>
2018-08-29 21:47:27 -0700

    comments added

--- src/test/java/day2/Day2.java
+++ src/test/java/day2/Day2.java
@@ -12,38 +12,63 @@ import org.testng.annotations.Test;
 
 public class Day2 {
 
+    //create a class field
     WebDriver driver;
 
 
     @BeforeTest
     public void beforeTest(){
+        //setting path to gecko driver
         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver.exe");
+
+        //creating instance of Firefox
         driver = new FirefoxDriver();
     }
 
     @Test
     public void testGoogleSearch() {
+        //creating object of By class using cssSelector
         By searchInputByCss = By.cssSelector("#lst-ib");
+
+        //string for search in Google
         String searchString = "Portnov";
+
+        //url
         String googleURL = "http://google.com";
 
+        //navigate to URL
         driver.get(googleURL);
+
+        //wait for element to be presented
         waitFor(searchInputByCss);
+
+        //sleep for 2 seconds
         sleepFor(2);
+
+        //type into search our string
         sendKeysTo(searchInputByCss, searchString);
     }
 
     private void sendKeysTo(By element, String charSequence){
+        //create webElement object by using findElement() which receives element
         WebElement webElement = driver.findElement(element);
+
+        //type charSequence
         webElement.sendKeys(charSequence);
+
+        //submit results
         webElement.submit();
     }
 
     private void waitFor(By element){
+        //create object wait with field driver and 10 seconds of timeout
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        //wait until element will presented on the page
         WebElement myDynamicElement = wait.until(ExpectedConditions.presenceOfElementLocated(element));
     }
 
+    //after test phase
     @AfterTest
     public void afterTest(){
         sleepFor(5);
@@ -51,6 +76,9 @@ public class Day2 {
     }
 
     private void sleepFor(int i) {
+
+        //try catch block Java
+        // https://docs.oracle.com/javase/tutorial/essential/exceptions/catch.html
         try {
             Thread.sleep(i * 1000);
         } catch (InterruptedException e) {
