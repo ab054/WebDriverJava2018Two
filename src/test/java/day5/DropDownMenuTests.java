aa6b736d15b89d3f1878f49085189d8e818c268a
ab054 <alfamiliya@gmail.com>
2018-09-19 18:29:37 -0700

    Dropdown menu tests

---
+++ src/test/java/day5/DropDownMenuTests.java
@@ -0,0 +1,70 @@
+package day5;
+
+import day4.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class DropDownMenuTests extends BaseTest {
+
+    @Test
+    public void dropDownTest() {
+
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+
+        WebElement dropdownList = driver.findElement(By.id("dropdown"));
+
+        List<WebElement> optionsFromList = dropdownList.findElements(By.tagName("option"));
+
+        for (int i = 0; i < optionsFromList.size(); i++) {
+
+            if (optionsFromList.get(i).getText().equals("Option 1")) {
+                optionsFromList.get(i).click();
+            }
+        }
+
+        String selectedOption = "";
+
+        for (int i = 0; i < optionsFromList.size(); i++) {
+            if (optionsFromList.get(i).isSelected()) {
+                selectedOption = optionsFromList.get(i).getText();
+            }
+        }
+
+        assertTrue(selectedOption.contains("Option 1"));
+    }
+
+    @Test
+    public void dropDownTestDiffWay() {
+
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+
+        Select dropDownList = new Select(driver.findElement(By.id("dropdown")));
+
+        dropDownList.selectByVisibleText("Option 1");
+
+        //dropDownList.selectByValue("1");
+
+        assertTrue(dropDownList.getFirstSelectedOption().getText().contains("Option 1"));
+    }
+
+
+    @Test
+    public void testDisabled() {
+
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+
+        Select dropdown = new Select(driver.findElement(By.id("dropdown")));
+
+        List<WebElement> allOptions = dropdown.getOptions();
+
+        assertTrue(!allOptions.get(0).isEnabled());
+
+    }
+
+}
